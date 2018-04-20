620f38e69d5397ae211823993efaee6f601df900
user_acronus <user_acronus@senacsp.edu.br>
2018-04-20 11:52:49 -0300

    lesson 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProgramaComissao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorMeta, perComissao, valorVenda, valorComissao;
+
+            Console.Clear();//Limpar a tela de console
+            Console.WriteLine("Digite o valor da meta");
+            valorMeta = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o percentual de comissao");
+            perComissao = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da venda");
+            valorVenda = double.Parse(Console.ReadLine());
+
+            if(valorVenda>=valorMeta)
+                valorComissao = valorVenda*(perComissao/100);
+            else
+                valorComissao = 0;
+
+            Console.WriteLine("O valor da comissão é: " + valorComissao);                  
+        
+        }
+    }
+}
